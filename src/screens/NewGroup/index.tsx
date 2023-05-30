92d089cb9f970f390bc6c68e85f65ec57d82da95
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2023-05-30 19:15:40 -0300

    feat: input component done

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header"
 import { Container, Content, Icon } from "./styles"
 import { Subtitle } from "@components/Subtitle"
 import { Button } from "@components/Button"
+import { Input } from "@components/Input"
 
 export function NewGroup() {
   return (
@@ -13,6 +14,7 @@ export function NewGroup() {
           title='Nova turma'
           subtitle='crie uma turma para adicionar pessoas'
         />
+        <Input placeholder='Nome da turma' />
         <Button title='Criar' />
       </Content>
     </Container>
