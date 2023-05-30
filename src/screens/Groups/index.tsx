5fcb7c5fadb8280aa3c252288038f8c16bef487f
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2023-05-30 12:04:45 -0300

    feat: using useState to render a group list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react"
+import { FlatList } from "react-native"
+
 import { Header } from "@components/Header"
 import { Container } from "./styles"
 import { Subtitle } from "@components/Subtitle"
 import { GroupCard } from "@components/GroupCard"
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera do futebol"])
+
   return (
     <Container>
       <Header />
       <Subtitle title='Turmas' subtitle='Jogue com sua turma' />
-      <GroupCard title='Galera' />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
